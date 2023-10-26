06d8c5d41e4824825c2abfdb048ff2fab7ddf560
yanis karibov <karibov@proitr.ru>
2023-10-26 22:40:50 +0300

    Init

---
+++ src/main/java/com/cyberpartizan/marketalert/bybitservice/ByBitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cyberpartizan.marketalert.bybitservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByBitServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ByBitServiceApplication.class, args);
+    }
+
+}
